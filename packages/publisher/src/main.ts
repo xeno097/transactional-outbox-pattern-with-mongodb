c9b5aec13a1d17226458c5df29e3f9890056bb2d
Sebastiano Faiella <xeno097.cp@gmail.com>
2022-04-26 13:04:06 -0400

    publisher: added logic to load config from .env file

--- packages/publisher/src/main.ts
+++ packages/publisher/src/main.ts
@@ -1,8 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { ConfigKeyEnum } from 'src/config/config-key.enum';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const PORT = configService.get(ConfigKeyEnum.PORT, 3000);
+
+  await app.listen(PORT);
 }
+
 bootstrap();
