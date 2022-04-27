609b67d625f0f3048529dcde9a59ad40636af1c6
Sebastiano Faiella <xeno097.cp@gmail.com>
2022-04-27 17:40:54 -0400

    subscriber: implemented the inbox module

---
+++ packages/subscriber/src/inbox/database/inbox.schema.ts
@@ -0,0 +1,35 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { BaseEvent, EventMetadata } from '../../../../core/lib';
+
+@Schema()
+export class Inbox extends Document implements BaseEvent {
+  @Prop({
+    required: true,
+  })
+  id: string;
+
+  @Prop({
+    required: true,
+  })
+  type: string;
+
+  @Prop({
+    type: Object,
+    required: true,
+  })
+  payload: Object;
+
+  @Prop({
+    required: true,
+  })
+  createdAt: string;
+
+  @Prop({
+    required: true,
+    type: EventMetadata,
+  })
+  metadata: EventMetadata;
+}
+
+export const InboxSchema = SchemaFactory.createForClass(Inbox);
