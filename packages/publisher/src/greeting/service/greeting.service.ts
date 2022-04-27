ba4baba3884da02d269eabb2335f443d3153f52c
Sebastiano Faiella <xeno097.cp@gmail.com>
2022-04-27 13:37:35 -0400

    publisher: updated the create greeting method to insert the created greeting event in the outbox collection

--- packages/publisher/src/greeting/service/greeting.service.ts
+++ packages/publisher/src/greeting/service/greeting.service.ts
@@ -1,20 +1,59 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { ConfigService } from '@nestjs/config';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Model, Connection } from 'mongoose';
+import { ConfigKeyEnum } from 'src/config/config-key.enum';
+import { Outbox } from 'src/outbox/database/outbox.schema';
+import { CreatedGreetingEvent } from '../../../../core/lib';
 import { Greeting } from '../database/greeting.schema';
 import { GreetingEntity } from './greeting.entity';
 
 @Injectable()
 export class GreetingService {
+  private readonly _serviceId: string;
+
   constructor(
+    private readonly configService: ConfigService,
+    @InjectConnection() private readonly connection: Connection,
+
     @InjectModel(Greeting.name) private readonly greetModel: Model<Greeting>,
-  ) {}
+    @InjectModel(Outbox.name) private readonly outBoxModel: Model<Outbox>,
+  ) {
+    const id = this.configService.get<string>(ConfigKeyEnum.SERVICE_ID);
+
+    if (!id) {
+      throw new Error('Service ID is not set');
+    }
+
+    this._serviceId = id;
+  }
 
   async create(greeting: GreetingEntity): Promise<GreetingEntity> {
-    const createdGreet = new this.greetModel(greeting);
+    const session = await this.connection.startSession();
 
-    await createdGreet.save();
+    session.startTransaction();
+    try {
+      const createdGreet = new this.greetModel(greeting);
 
-    return greeting;
+      const event = new CreatedGreetingEvent(greeting, {
+        createdBy: this._serviceId,
+      });
+
+      const outbox = new this.outBoxModel(event);
+
+      await Promise.all([
+        createdGreet.save({ session }),
+        outbox.save({ session }),
+      ]);
+
+      await session.commitTransaction();
+
+      return greeting;
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    } finally {
+      await session.endSession();
+    }
   }
 }
